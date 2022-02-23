54be109000b94b0a28d07ed47a0362426545380f
bbermudes <99336138+bbermudes@users.noreply.github.com>
2022-02-23 12:14:02 -0300

    Create codigo.ino

---
+++ aula 3/codigo.ino
@@ -0,0 +1,37 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(6, HIGH);
+  digitalWrite(3, HIGH);
+  delay(1000);
+  digitalWrite(6, LOW);
+  delay(100);
+  
+  digitalWrite(5, HIGH);
+  delay(1000);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  delay(100);
+  
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  delay(1000);
+  digitalWrite(3, LOW);
+  delay(100);
+  
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  delay(100);
+}
